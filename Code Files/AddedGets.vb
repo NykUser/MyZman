a36c572752dc5c5f6d6ed014970d5b07b31a4a5d
NykUser <3620950@gmail.com>
2021-08-24 20:09:13 -0400

    Fix to  ZmanGet's

--- Code Files/AddedGets.vb
+++ Code Files/AddedGets.vb
@@ -3,7 +3,7 @@
     Private TempCZC As ComplexZmanimCalendar
     Public Function ZmanGetMisheyakir50(DateIn As Date, LocationIn As GeoLocation)
         TempCZC = New ComplexZmanimCalendar(DateIn, LocationIn)
-        If Frminfo.mUseUSNO.Checked = False Then
+        If varSC.UseOlderUsnoAlgorithm = False Then
             TempCZC.AstronomicalCalculator = New NOAACalculator()
         Else
             TempCZC.AstronomicalCalculator = New ZmanimCalculator()
@@ -16,6 +16,12 @@
         End Try
     End Function
     Public Function ZmanGetPlagHamincha8Point5Degrees(DateIn As Date, LocationIn As GeoLocation)
+        TempCZC = New ComplexZmanimCalendar(DateIn, LocationIn)
+        If varSC.UseOlderUsnoAlgorithm = False Then
+            TempCZC.AstronomicalCalculator = New NOAACalculator()
+        Else
+            TempCZC.AstronomicalCalculator = New ZmanimCalculator()
+        End If
         Try
             Dim mydate As Time
             'looks fine now - the was some kind of reason for adding time formats in the past
@@ -25,6 +31,12 @@
         End Try
     End Function
     Public Function ZmanGetBeinHashmases8Point5Degrees13Min(DateIn As Date, LocationIn As GeoLocation)
+        TempCZC = New ComplexZmanimCalendar(DateIn, LocationIn)
+        If varSC.UseOlderUsnoAlgorithm = False Then
+            TempCZC.AstronomicalCalculator = New NOAACalculator()
+        Else
+            TempCZC.AstronomicalCalculator = New ZmanimCalculator()
+        End If
         Try
             Return CDate(TempCZC.GetSunsetOffsetByDegrees(90 + 8.5).ToString).AddMinutes(-13.5) ' ToLongTimeString
         Catch ex As Exception
@@ -32,6 +44,12 @@
         End Try
     End Function
     Public Function ZmanGetBeinHashmases8Point5Degrees5Point3(DateIn As Date, LocationIn As GeoLocation)
+        TempCZC = New ComplexZmanimCalendar(DateIn, LocationIn)
+        If varSC.UseOlderUsnoAlgorithm = False Then
+            TempCZC.AstronomicalCalculator = New NOAACalculator()
+        Else
+            TempCZC.AstronomicalCalculator = New ZmanimCalculator()
+        End If
         Try
             Dim myTimeSpan = CDate(TempCZC.GetTzaisGeonim8Point5Degrees().ToString) - CDate(TempCZC.GetSunset().ToString)
             Return CDate(TempCZC.GetSunsetOffsetByDegrees(90 + 8.5).ToString).AddMilliseconds(-(myTimeSpan.TotalMilliseconds / 5.3333)) '.ToLongTimeString
