9cba0d749601259a9a5025c59b547d7c23b124f0
NykUser <3620950@gmail.com>
2021-08-24 20:23:23 -0400

    FrmExport simplified

--- Code Files/FrmExport.vb
+++ Code Files/FrmExport.vb
@@ -2,9 +2,9 @@
 Imports Microsoft.WindowsAPICodePack.Dialogs
 Imports Microsoft.WindowsAPICodePack.Shell
 Public Class FrmExport
-    Private Sub ExportQuestion_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub FrmExport_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.Location = New System.Drawing.Point(Frminfo.Location.X + ((Frminfo.Width - Me.Width) / 2), Frminfo.Bounds.Bottom - (Me.Height + 45))
-        If Frminfo.mHebrewMenus.Checked = True Then
+        If varSC.HebrewMenus = True Then
             Me.Text = "ייצוא"
             Label1.Text = "ימים לייצוא"
             Label2.Text = "ימים לכל קובץ (vcs)"
@@ -12,18 +12,6 @@ Public Class FrmExport
             Label2.RightToLeft = 1
             btCSV.Text = "לאקסל"
             btVCS.Text = "VCS"
-        Else
-            Label1.Text = "Days To Export"
-            Label2.Text = "Days Per File (vcs)"
-            Label1.RightToLeft = 0
-            Label2.RightToLeft = 0
-            btCSV.Text = "To Excel"
-            btVCS.Text = "To VCS"
-        End If
-        tbDays.Text = 365
-        tbPer.Text = 365
-
-        If varSC.HebrewMenus = True Then
             Try
                 Label1.Font = MemoryFonts.GetFont(1, 8.75, FontStyle.Regular)
                 Label2.Font = MemoryFonts.GetFont(1, 8.75, FontStyle.Regular)
@@ -32,6 +20,12 @@ Public Class FrmExport
             Catch ex As Exception
             End Try
         Else
+            Label1.Text = "Days To Export"
+            Label2.Text = "Days Per File (vcs)"
+            Label1.RightToLeft = 0
+            Label2.RightToLeft = 0
+            btCSV.Text = "To Excel"
+            btVCS.Text = "To VCS"
             Try
                 Label1.Font = MemoryFonts.GetFont(1, 7.75, FontStyle.Regular)
                 Label2.Font = MemoryFonts.GetFont(1, 7.75, FontStyle.Regular)
@@ -41,7 +35,8 @@ Public Class FrmExport
             End Try
         End If
 
-
+        tbDays.Text = 365
+        tbPer.Text = 365
     End Sub
     Private Sub btCancel_Click(sender As Object, e As EventArgs) Handles btCancel.Click
         Me.Close()
@@ -57,50 +52,10 @@ Public Class FrmExport
     Private Sub tbDays_TextChanged(sender As Object, e As EventArgs) Handles tbDays.TextChanged
         If tbDays.Text < tbPer.Text Then tbPer.Text = tbDays.Text
     End Sub
-
     Private Sub btVCS_Click(sender As Object, e As EventArgs) Handles btVCS.Click
-        If IsNumeric(tbDays.Text) = False Or IsNumeric(tbPer.Text) = False Or tbDays.Text = 0 Or tbPer.Text = 0 Then
-            Using New Centered_MessageBox(Me, "ParentCenter")
-                If varSC.HebrewMenus = True Then
-                    MsgBox("!מידע על ימים לייצוא חסרה", MsgBoxStyle.OkOnly + MsgBoxStyle.Information + MsgBoxStyle.MsgBoxRight)
-                Else
-                    MsgBox("Missing Days To Export Info!", MsgBoxStyle.OkOnly + MsgBoxStyle.Information)
-                End If
-            End Using
-            Exit Sub
-        End If
-
-        If varSC.Zmanim.Count < 1 Then
-            Using New Centered_MessageBox(Me, "ParentCenter")
-                If varSC.HebrewMenus = True Then
-                    MsgBox("!רשימת הזמנים חסרה", MsgBoxStyle.OkOnly + MsgBoxStyle.Information + MsgBoxStyle.MsgBoxRight)
-                Else
-                    MsgBox("Missing Zmanim List!", MsgBoxStyle.OkOnly + MsgBoxStyle.Information)
-                End If
-            End Using
-            Exit Sub
-        End If
-
-        Dim folder As String
-        'Dim MY_FolderBrowser As New FolderBrowserDialog
-        'MY_FolderBrowser.ShowNewFolderButton = True
-        'MY_FolderBrowser.SelectedPath = My.Computer.FileSystem.SpecialDirectories.Desktop
-        Dim MY_FolderBrowser As CommonOpenFileDialog = New CommonOpenFileDialog()
-        MY_FolderBrowser.Multiselect = False
-        MY_FolderBrowser.IsFolderPicker = True
-        MY_FolderBrowser.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.Desktop
-
-        Using New Centered_MessageBox(Me, "ParentCenter")
-            'If MY_FolderBrowser.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
-            If MY_FolderBrowser.ShowDialog() = CommonFileDialogResult.OK Then
-                'folder = MY_FolderBrowser.SelectedPath Then
-                folder = MY_FolderBrowser.FileName
-            Else
-                Exit Sub
-            End If
-        End Using
-
-        'folder = My.Computer.FileSystem.SpecialDirectories.Desktop & "\New folder"
+        If fCheckInfoNeeded() = False Then Exit Sub
+        Dim folder As String = fGetFolder()
+        If folder = "" Then Exit Sub
 
         Dim utf8WithoutBom As New System.Text.UTF8Encoding(False)
         Dim file As System.IO.StreamWriter
@@ -181,8 +136,70 @@ Public Class FrmExport
         Me.Close()
     End Sub
     Private Sub btCSV_Click(sender As Object, e As EventArgs) Handles btCSV.Click
+        If fCheckInfoNeeded() = False Then Exit Sub
+        Dim folder As String = fGetFolder()
+        If folder = "" Then Exit Sub
 
-        If IsNumeric(tbDays.Text) = False Or tbDays.Text = 0 Then
+        Dim File As System.IO.StreamWriter
+        Dim FileFullPath As String = folder & "\MyZman VCS Export Lat " & varGeoLocation.Latitude & " long " & varGeoLocation.Longitude & ".csv" '& Now.ToString("M-d-yy H.m")
+
+        Dim timeFormat As String = "h:mm:ss tt"
+        If varSC.Clock24Hour = True Then timeFormat = "H:mm:ss"
+        Dim MyZman As Date
+        Dim Work_Date As Date = Frminfo.dpEngdate.Value
+        Dim NumDays As Integer = tbDays.Text
+        Dim ResultArray
+        Dim i As Integer = 0
+        Dim Args() ' for AddedGets
+        Dim TempLocation As GeoLocation = varGeoLocation
+        Dim TempCZC As ComplexZmanimCalendar
+
+        File = My.Computer.FileSystem.OpenTextFileWriter(FileFullPath, False, System.Text.Encoding.UTF8) 'System.Text.Encoding.UTF8
+        Dim RowString As String
+        Dim HeaderString As String = "Date,תאריך,פרשה,יום טוב,דף היומי,"
+
+        For Each Z In varSC.Zmanim
+            HeaderString = HeaderString & Z.DisplayName & ","
+        Next
+
+        File.WriteLine(HeaderString)
+
+        For i = 1 To NumDays
+            TempCZC = New ComplexZmanimCalendar(Work_Date, TempLocation)
+            ResultArray = Get_HebDate(Work_Date)
+            RowString = Work_Date.ToShortDateString & "," & ResultArray(0) & "," & ResultArray(1) & "," & ResultArray(2) & "," & ResultArray(3) & ","
+
+            For Each Z In varSC.Zmanim
+                If InStr(Z.FunctionName, "ShaahZmanis") > 0 Then
+                    Try
+                        MyZman = #12:00:00 AM#.AddMilliseconds(CallByName(TempCZC, Z.FunctionName, CallType.Get))
+                        RowString = RowString & MyZman.ToString("H:mm:ss") & ","
+                    Catch ex As Exception
+                    End Try
+                Else
+                    Try
+                        If Z.ObjectName = "varCZC" Then
+                            MyZman = CDate(CallByName(TempCZC, Z.FunctionName, CallType.Get)).ToString(timeFormat)
+                        Else
+                            Args = {Work_Date, TempLocation} ' for AddedGets
+                            MyZman = CDate(CallByName(varAddedGets, Z.FunctionName, CallType.Get, Args))
+                        End If
+                        RowString = RowString & MyZman.ToString(timeFormat) & ","
+                    Catch ex As Exception
+                    End Try
+                End If
+            Next
+            File.WriteLine(RowString)
+
+            Work_Date = Work_Date.AddDays(1)
+        Next
+        File.Close()
+
+        Process.Start(FileFullPath)
+        Me.Close()
+    End Sub
+    Private Function fCheckInfoNeeded() As Boolean
+        If IsNumeric(tbDays.Text) = False Or IsNumeric(tbPer.Text) = False Or tbDays.Text = 0 Or tbPer.Text = 0 Then
             Using New Centered_MessageBox(Me, "ParentCenter")
                 If varSC.HebrewMenus = True Then
                     MsgBox("!מידע על ימים לייצוא חסרה", MsgBoxStyle.OkOnly + MsgBoxStyle.Information + MsgBoxStyle.MsgBoxRight)
@@ -190,7 +207,7 @@ Public Class FrmExport
                     MsgBox("Missing Days To Export Info!", MsgBoxStyle.OkOnly + MsgBoxStyle.Information)
                 End If
             End Using
-            Exit Sub
+            Return False
         End If
 
         If varSC.Zmanim.Count < 1 Then
@@ -201,9 +218,11 @@ Public Class FrmExport
                     MsgBox("Missing Zmanim List!", MsgBoxStyle.OkOnly + MsgBoxStyle.Information)
                 End If
             End Using
-            Exit Sub
+            Return False
         End If
-
+        Return True
+    End Function
+    Private Function fGetFolder() As String
         Dim folder As String
         'Dim MY_FolderBrowser As New FolderBrowserDialog
         'MY_FolderBrowser.ShowNewFolderButton = True
@@ -219,69 +238,9 @@ Public Class FrmExport
                 'folder = MY_FolderBrowser.SelectedPath Then
                 folder = MY_FolderBrowser.FileName
             Else
-                Exit Sub
+                Return ""
             End If
         End Using
-
-        'folder = My.Computer.FileSystem.SpecialDirectories.Desktop & "\New folder"
-
-        Dim File As System.IO.StreamWriter
-            Dim FileFullPath As String = folder & "\MyZman VCS Export Lat " & varGeoLocation.Latitude & " long " & varGeoLocation.Longitude & ".csv" '& Now.ToString("M-d-yy H.m")
-
-            Dim timeFormat As String = "h:mm:ss" ' no am pm RTL
-            If varSC.Clock24Hour = True Then timeFormat = "H:mm:ss"
-            Dim MyZman As Date
-            Dim Work_Date As Date = Frminfo.dpEngdate.Value
-            Dim NumDays As Integer = tbDays.Text
-            Dim ResultArray
-            Dim i As Integer = 0
-            Dim Args() ' for AddedGets
-            Dim TempLocation As GeoLocation = varGeoLocation
-            Dim TempCZC As ComplexZmanimCalendar
-
-            File = My.Computer.FileSystem.OpenTextFileWriter(FileFullPath, False, System.Text.Encoding.UTF8) 'System.Text.Encoding.UTF8
-            Dim RowString As String
-            Dim HeaderString As String = "Date,תאריך,פרשה,יום טוב,דף היומי,"
-
-            For Each Z In varSC.Zmanim
-                HeaderString = HeaderString & Z.DisplayName & ","
-            Next
-
-            File.WriteLine(HeaderString)
-
-            For i = 1 To NumDays
-                TempCZC = New ComplexZmanimCalendar(Work_Date, TempLocation)
-                ResultArray = Get_HebDate(Work_Date)
-                RowString = Work_Date.ToShortDateString & "," & ResultArray(0) & "," & ResultArray(1) & "," & ResultArray(2) & "," & ResultArray(3) & ","
-
-                For Each Z In varSC.Zmanim
-                    If InStr(Z.FunctionName, "ShaahZmanis") > 0 Then
-                        Try
-                            MyZman = #12:00:00 AM#.AddMilliseconds(CallByName(TempCZC, Z.FunctionName, CallType.Get))
-                            RowString = RowString & MyZman.ToString("H:mm:ss") & ","
-                        Catch ex As Exception
-                        End Try
-                    Else
-                        Try
-                            If Z.ObjectName = "varCZC" Then
-                                MyZman = CDate(CallByName(TempCZC, Z.FunctionName, CallType.Get)).ToString(timeFormat)
-                            Else
-                                Args = {Work_Date, TempLocation} ' for AddedGets
-                                MyZman = CDate(CallByName(varAddedGets, Z.FunctionName, CallType.Get, Args))
-                            End If
-                            RowString = RowString & MyZman.ToString(timeFormat) & ","
-                        Catch ex As Exception
-                        End Try
-                    End If
-                Next
-                File.WriteLine(RowString)
-
-                Work_Date = Work_Date.AddDays(1)
-            Next
-            File.Close()
-
-            Process.Start(FileFullPath)
-            Me.Close()
-    End Sub
-
+        Return folder
+    End Function
 End Class
